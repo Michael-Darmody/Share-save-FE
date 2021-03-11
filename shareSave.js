3f87a20906eeb5163e4477d484d620cb1cc5584c
Michael-Darmody <darmodymichael@gmail.com>
2021-03-11 15:31:47 +0000

    Refactored JS to format shares into cards when rendering

--- shareSave.js
+++ shareSave.js
@@ -14,16 +14,39 @@ function getShares() {
 }
 
 function renderShare(share) {
+
+    const newColumn = document.createElement("section");
+    newColumn.className = "col";
+
     const newShare = document.createElement("section");
-    const attrP = document.createElement("p");
+    newShare.className = "card";
+    newColumn.appendChild(newShare);
 
-    attrP.innerHTML = "Name: " + share.name + "<br>";
-    attrP.innerHTML += "Amount: " + share.amount + "<br>";
-    attrP.innerHTML += "Share price: £" + share.price + "<br>";
-    attrP.innerHTML += "Value: £" + share.amount * share.price;
-    newShare.appendChild(attrP);
+    const shareBody = document.createElement("section");
+    shareBody.className = "card-body";
+    newShare.appendChild(shareBody);
 
-    return newShare;
+    const shareName = document.createElement("h5");
+    shareName.className = "card-title";
+    shareName.innerText = share.name;
+    shareBody.appendChild(shareName);
+
+    const shareText = document.createElement("p");
+    shareText.className = "card-text";
+    shareText.innerHTML = "Name: " + share.name;
+    shareText.innerHTML += "<br>";
+    shareText.innerHTML += "Amount: " + share.amount;
+    shareText.innerHTML += "<br>";
+    shareText.innerHTML += "Share price:  £" + share.price;
+    shareText.innerHTML += "<br>";
+    shareText.innerHTML += "Value:  £" + share.amount * share.price;
+    shareBody.appendChild(shareText);
+
+    const shareFooter = document.createElement("section");
+    shareFooter.className = "card-footer"
+    newShare.appendChild(shareFooter);
+
+    return newColumn;
 }
 
 getShares();
